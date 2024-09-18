286f07587b25dbed4d9499b0b9d247ef51e00d2f
KodcuAhmet <ahmet.cigil@tuta.com>
2024-09-18 15:58:03 +0200

    124. Data-Driven Testing in Playwright.NUnit

--- PlaywrightNUnitDemo/UnitTest1.cs
+++ PlaywrightNUnitDemo/UnitTest1.cs
@@ -14,20 +14,29 @@ namespace PlaywrightNUnitDemo
         }
 
         [Test]
-        public async Task Test1()
+        [TestCaseSource(nameof(Login))]
+        public async Task Test1(LoginModel login)
         {
             await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
             await Page.GetByLabel("UserName").DblClickAsync();
             await Page.GetByLabel("UserName").ClickAsync();
-            await Page.GetByLabel("UserName").FillAsync("admin");
-            await Page.GetByLabel("UserName").PressAsync("Tab");
-            await Page.GetByLabel("Password").FillAsync("password");
+            await Page.GetByLabel("UserName").FillAsync(login.UserName);
+            await Page.GetByLabel("Password").FillAsync(login.Password);
             await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
             await Page.GetByRole(AriaRole.Link, new() { Name = "Employee List" }).ClickAsync();
             await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
             await Page.GetByRole(AriaRole.Heading, new() { Name = "Employee" }).ClickAsync();
             await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Employee" })).ToBeVisibleAsync();
 
+        }
+
+        public static IEnumerable<LoginModel> Login()
+        {
+            yield return new LoginModel()
+            {
+                UserName = "admin",
+                Password = "password"
+            };
         }
     }
 }
